d6f712a64a79dfa279d8e51d7a64d9ef3f19ffb8
orichason <orichason@gmail.com>
2022-09-08 14:26:40 -0700

    finish array

---
+++ ArrayQueue.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Queues
+{
+    public class ArrayQueue<T>
+    {
+        public int Count { get; private set; }
+        private T[] data;
+        private int head;
+        private int tail;
+
+        public ArrayQueue()
+        {
+            Count = 0;
+            data = new T[4];
+            head = 0;
+            tail = 0;
+        }
+
+        private void Resize()
+        {
+            T[] newArray = new T[data.Length * 2];
+
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                newArray[i] = data[i];
+            }
+
+            data = newArray;
+        }
+
+        public void Enqueue(T value)
+        {
+            Count++;
+            data[tail] = value;
+            if(Count > data.Length)
+            {
+
+            }
+        }
+    }
+}
